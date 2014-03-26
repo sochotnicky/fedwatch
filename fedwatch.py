d39e98dbb563b438dcca5d646ab12381b7370ea8
Stanislav Ochotnicky <sochotnicky@redhat.com>
2014-03-26 11:45:40 +0100

    Raise version for next release

--- fedwatch.py
+++ fedwatch.py
@@ -20,7 +20,7 @@ Module for watching fedmsg messages and running scripts based on those messages
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
     USA
 """
-__version__='0.3'
+__version__='0.4'
 
 import os
 import logging
